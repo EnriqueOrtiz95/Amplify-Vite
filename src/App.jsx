c3b618f354a97456a31f756950b8fef9e16b20b1
EnriqueOrtiz95 <enortizlo@gmail.com>
2023-03-04 13:25:16 -0700

    testing amplify with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amplify + VITE</h1>
+    </div>
+  )
+}
+
+export default App
